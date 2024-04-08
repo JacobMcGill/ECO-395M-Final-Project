74b090f3b66344b0d0cd3f607cff1c9f62689679
JacobMcGill <jacobpmcgill97@gmail.com>
2024-04-07 19:24:56 -0500

    Added files

--- First pass at cleaning data.R
+++ First pass at cleaning data.R
@@ -1,9 +1,9 @@
 library(readxl)
 library(tidyverse)
 library(dplyr)
-Energy_data <- read_excel("C:/Users/jacob/Downloads/HS861M 2010-.xlsx")
-Oil_Employment_data <- read.csv("C:/Users/jacob/Downloads/CES1021100001.csv")
-US_Crude_Oil <- read_csv("C:/Users/jacob/Downloads/U.S._Field_Production_of_Crude_Oil.csv", skip = 4)
+Energy_data <- read_excel("C:/Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/HS861M 2010-.xlsx")
+Oil_Employment_data <- read.csv("C://Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/Data_Mining_project/CES1021100001.csv")
+US_Crude_Oil <- read_csv("C://Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/Data_Mining_project/U.S._Field_Production_of_Crude_Oil.csv", skip = 4)
 # Cleaning the data and focusing just on totals
 Energy_data = Energy_data[-c(1),-c(5:20)]
 names(Energy_data) = Energy_data[1,]
@@ -11,9 +11,12 @@ Energy_data = Energy_data[-c(1),]
 Energy_data = Energy_data %>%
   mutate(DATE = as.Date(paste(Year, Month, 01, sep = "-")))
 Oil_Employment_data = Oil_Employment_data %>%
-  mutate(DATE = as.Date(DATE))
+  mutate(DATE = as.Date(DATE)) %>%
+  rename("oil_emp(thousands)" = CES1021100001)
 US_Crude_Oil = US_Crude_Oil %>%
   mutate(DATE = as.Date(paste0(substring(Month, 1, 3), "01", substring(Month, 5)), format = "%b %d %Y"))
+  
 #Now work on joing data
 joined = inner_join(Energy_data, Oil_Employment_data, by = "DATE")
 joined = subset((left_join(joined, US_Crude_Oil, by = "DATE")),select = -c(Month.y))  
+write.csv(joined, "C://Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/Modified_data/joined_data.csv")
--- Temp_upload.R
+++ Temp_upload.R
@@ -1,6 +1,6 @@
 library(readr)
 library(tidyverse)
-temp_folder = "C://Users/jacob/Downloads/Data_Mining_project/Temp_data"
+temp_folder = "C://Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/Data_Mining_project/Temp_data"
 # List all CSV files in the directory
 csv_files <- list.files(path = temp_folder, pattern = "\\.csv$", full.names = TRUE)
 
@@ -25,4 +25,5 @@ for (csv_file in csv_files) {
 }
 
 # Merge all data frames into a single data frame
-merged_df <- bind_rows(dfs)
+state_temp_data <- bind_rows(dfs)
+write.csv(state_temp_data, "C://Users/jacob/Downloads/Data_Mining_project/ECO-395M-Final-Project/Modified_data/state_temp_data.csv")
