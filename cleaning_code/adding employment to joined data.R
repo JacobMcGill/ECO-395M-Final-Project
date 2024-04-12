43e2a134a10a43299cc5f2a86ae1f93a1d078d02
doliner10 <doliner@utexas.edu>
2024-04-12 09:25:43 -0500

    adding employment join R script

---
+++ cleaning_code/adding employment to joined data.R
@@ -0,0 +1,16 @@
+library(readxl)
+library(tidyverse)
+library(dplyr)
+
+employment_data <- read_csv("/Users/danieloliner/Downloads/monthlyemployment.csv")
+joined_data <- read_csv("/Users/danieloliner/Downloads/joined_data.csv")
+
+employment_data <- employment_data %>%
+  mutate(DATE = as.Date(paste(Year, Month, "01", sep = "-")))
+
+final_joined <- inner_join(joined_data, employment_data, by = c("DATE", "State" = "State Abbrev"))
+
+final_joined <- final_joined %>%
+  select(-Month, -Year.y, -State.y)
+
+write.csv(final_joined, "/Users/danieloliner/Documents/GitHub/ECO-395M-Final-Project/Modified_data/final_joined.csv", row.names = FALSE)
