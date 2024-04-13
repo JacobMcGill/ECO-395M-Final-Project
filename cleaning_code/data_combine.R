ec4cb22862216346e6d2677fb5f5e1f2e9820b48
JacobMcGill <jacobpmcgill97@gmail.com>
2024-04-13 16:46:12 -0500

    Made sure temp was joined to dataframe

--- cleaning_code/data_combine.R
+++ cleaning_code/data_combine.R
@@ -12,8 +12,8 @@ UR_data = read_csv("C://Users/jacob/OneDrive/Documents/ECO-395M-Final-Project/Mo
          Year = year(as.Date(DATE)))
 Temp_data = read_csv("C://Users/jacob/OneDrive/Documents/ECO-395M-Final-Project/Modified_data/state_temp_data.csv")%>% 
   select(-1) %>%
-  mutate(Month = substr(as.character(Date), 1, 4),
-         Year = as.integer(substr(as.character(Date), 5, 6)))
+  mutate(Year = as.numeric(substr(as.character(Date), 1, 4)),
+         Month = as.numeric(substr(as.character(Date), 5, 6)))
 gen_monthly = read_csv("C://Users/jacob/OneDrive/Documents/ECO-395M-Final-Project/Modified_data/gen_monthly.csv")%>% 
   select(-1) %>%
   rename(Month = MONTH,
@@ -23,8 +23,9 @@ final_monthly_join = inner_join(final_joined, gen_monthly, by = c("Year", "Month
 final_UR_join = inner_join(final_monthly_join, UR_data, by = c("Year", "Month", "State")) %>%
   select(-26) %>%
   rename(Date = DATE.x)
-final_temp_join = inner_join(final_UR_join, UR_data, by = c("Year", "Month", "State")) %>%
+final_temp_join = inner_join(final_UR_join, Temp_data, by = c("Year", "Month", "State")) %>%
   select(-26, -27) %>%
-  rename(UR = UR_Rate.y)
+  rename(Temp = Value,
+         Temp_Anomaly = Anomaly)
 final_data = final_temp_join
 write.csv(final_data, "C://Users/jacob/OneDrive/Documents/ECO-395M-Final-Project/Modified_data/final_data.csv")
