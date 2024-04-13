e78eb539467901f11befa1b78c36dba70d64e779
JacobMcGill <jacobpmcgill97@gmail.com>
2024-04-13 16:55:14 -0500

    Modified final data

--- cleaning_code/data_combine.R
+++ cleaning_code/data_combine.R
@@ -24,8 +24,7 @@ final_UR_join = inner_join(final_monthly_join, UR_data, by = c("Year", "Month", 
   select(-26) %>%
   rename(Date = DATE.x)
 final_temp_join = inner_join(final_UR_join, Temp_data, by = c("Year", "Month", "State")) %>%
-  select(-26, -27) %>%
-  rename(UR = UR_Rate.y)
+  select(-26, -27)
 final_data = final_temp_join %>% 
   rename(Temp = Value,
          Temp_Anomaly = Anomaly)
